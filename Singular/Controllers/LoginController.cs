bcc21dbe04d2a09e3f607661394b314ee207e84c
CPerlato <carolina.perlato17@gmail.com>
2023-09-27 16:51:46 -0300

    Inserindo a pagina Login

---
+++ Singular/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Singular.Models;
+
+namespace Singular.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Entrar(LoginModel loginModel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {              
+                    return RedirectToAction("Index","Home");
+                }
+
+                return View("Index");
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Login invalido, tente novamente, detalhe do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ Singular/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Singular.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Digite o login")]
+        public string? Email { get; set; }
+     
+        [Required(ErrorMessage = "Digite sua senha")]
+        public string? Senha { get; set; }
+    }
+}
