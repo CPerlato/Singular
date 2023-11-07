42e0f57f3e4cf3af2a04cdc89a1adc52bad55678
CAROLINA FERRARI <carolina.ferrari@fatec.sp.gov.br>
2023-11-07 13:53:47 -0300

    Inserindo a pag Ranking e Perfil

---
+++ Singular/Controllers/ContaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Singular.Controllers
+{
+    public class ContaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Singular/Controllers/RankingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Singular.Controllers
+{
+    public class RankingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
